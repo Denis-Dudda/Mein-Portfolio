dfa65cab99542e716fcc44d44e28ce7dcb04fffd
Denis-Dudda <denis.d@hotmail.de>
2024-12-24 19:17:14 +0100

    bug fix public folder

--- src/app/my-skill-set/my-skill-set.component.ts
+++ src/app/my-skill-set/my-skill-set.component.ts
@@ -14,16 +14,16 @@ import { Component } from '@angular/core';
 export class MySkillSetComponent {
 
   icons = [
-    { src: 'assets/icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
-    { src: 'assets/icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
-    { src: 'assets/icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
-    { src: 'assets/icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
-    { src: 'assets/icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
-    { src: 'assets/icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
-    { src: 'assets/icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
-    { src: 'assets/icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
-    { src: 'assets/icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
-    { src: 'assets/icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
-    { src: 'assets/icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
+    { src: '/icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
+    { src: '/icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
+    { src: '/icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
+    { src: '/icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
+    { src: '/icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
+    { src: '/icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
+    { src: '/icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
+    { src: '/icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
+    { src: '/icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
+    { src: '/icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
+    { src: '/icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
   ];
 }

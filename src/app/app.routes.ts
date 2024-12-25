2200e5a071f19bcc9ae8bec5090583f0efe89b53
Denis-Dudda <denis.d@hotmail.de>
2024-12-25 12:03:37 +0100

    nav links working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { HeroComponent } from "./main-page/hero/hero.component";
-import { MyWorkComponent } from "./main-page/my-work/my-work.component";
 import { MainPageComponent } from './main-page/main-page.component';
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
-    
-    
 ];

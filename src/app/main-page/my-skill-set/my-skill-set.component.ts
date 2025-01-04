b6428da55421109ec8960ea908ce00c0ab376092
Denis-Dudda <denis.d@hotmail.de>
2025-01-04 14:48:55 +0100

    bug fix

--- src/app/main-page/my-skill-set/my-skill-set.component.ts
+++ src/app/main-page/my-skill-set/my-skill-set.component.ts
@@ -15,16 +15,16 @@ import { TranslateModule } from '@ngx-translate/core';
 export class MySkillSetComponent {
 
   icons = [
-    { src: '/icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
-    { src: '/icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
-    { src: '/icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
-    { src: '/icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
-    { src: '/icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
-    { src: '/icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
-    { src: '/icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
-    { src: '/icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
-    { src: '/icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
-    { src: '/icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
-    { src: '/icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
+    { src: './icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
+    { src: './icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
+    { src: './icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
+    { src: './icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
+    { src: './icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
+    { src: './icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
+    { src: './icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
+    { src: './icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
+    { src: './icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
+    { src: './icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
+    { src: './icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
   ];
 }

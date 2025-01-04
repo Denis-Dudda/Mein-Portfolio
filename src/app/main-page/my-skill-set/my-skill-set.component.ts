bd66c384109fc17830aa4924e076b81963ae74a4
Denis-Dudda <denis.d@hotmail.de>
2025-01-04 11:47:05 +0100

    skills translate en

--- src/app/main-page/my-skill-set/my-skill-set.component.ts
+++ src/app/main-page/my-skill-set/my-skill-set.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-skill-set',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './my-skill-set.component.html',
   styleUrl: './my-skill-set.component.scss'
 })

a5900ed28ab122e7a8982bbbd0984fc2b581c9f5
Denis-Dudda <denis.d@hotmail.de>
2025-01-05 10:34:18 +0100

    work on contact form

--- src/app/main-page/contact-me/contact-me.component.ts
+++ src/app/main-page/contact-me/contact-me.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule,
+            FormsModule
+  ],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
+  onSubmit() {
+    console.log('läuft');
+    
+  }
 }

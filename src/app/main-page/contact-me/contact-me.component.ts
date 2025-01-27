3535c155d5f3151fd6449098c7940cb112bd44c9
Denis-Dudda <denis.d@hotmail.de>
2025-01-27 18:32:33 +0100

    add php for contact

--- src/app/main-page/contact-me/contact-me.component.ts
+++ src/app/main-page/contact-me/contact-me.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { AnimationService } from '../../services/animation.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-me',
@@ -14,9 +15,44 @@ import { AnimationService } from '../../services/animation.service';
 })
 export class ContactMeComponent {
 
-  onSubmit() {
-    console.log('läuft');
-    
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email:"",
+    message: ""
+  }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
   
   constructor (private animationService: AnimationService) {

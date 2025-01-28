6273b192eaa1447b76d97575cf9e9980e681e6d1
Denis-Dudda <denis.d@hotmail.de>
2025-01-28 18:17:20 +0100

    picture bug fix

--- src/app/main-page/my-skill-set/my-skill-set.component.ts
+++ src/app/main-page/my-skill-set/my-skill-set.component.ts
@@ -18,17 +18,17 @@ export class MySkillSetComponent implements AfterViewInit{
   @ViewChild('skill') skill!: ElementRef;  // Hier referenzieren wir das Element, das beobachtet werden soll 
 
   icons = [
-    { src: './icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
-    { src: './icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
-    { src: './icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
-    { src: './icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
-    { src: './icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
-    { src: './icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
-    { src: './icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
-    { src: './icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
-    { src: './icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
-    { src: './icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
-    { src: './icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
+    { src: './img/icons/Property 1.svg', label: 'Icon 1', property: 'Angular' },
+    { src: './img/icons/Property 2.svg', label: 'Icon 2', property: 'TypeScript' },
+    { src: './img/icons/Property 3.svg', label: 'Icon 3', property: 'JavaScript' },
+    { src: './img/icons/Property 4.svg', label: 'Icon 4', property: 'HTML' },
+    { src: './img/icons/Property 5.svg', label: 'Icon 5', property: 'CSS' },
+    { src: './img/icons/Property 6.svg', label: 'Icon 6', property: 'Rest-Api' },
+    { src: './img/icons/Property 7.svg', label: 'Icon 7', property: 'Firebase' },
+    { src: './img/icons/Property 8.svg', label: 'Icon 8', property: 'Git' },
+    { src: './img/icons/Property 9.svg', label: 'Icon 9', property: 'Scrum' },
+    { src: './img/icons/Property 10.svg', label: 'Icon 10', property: 'Material design' },
+    { src: './img/icons/Property 11.svg', label: 'Icon 11', property: 'Challenge me' }
   ];
 
   constructor (private animationService: AnimationService) {

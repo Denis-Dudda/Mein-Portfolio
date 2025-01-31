4de5131722ecaf5c79cf45594bb24d97ba7626b9
Denis-Dudda <denis.d@hotmail.de>
2025-01-31 13:19:09 +0100

    bug fix

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AnimationService } from '../../services/animation.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+    constructor (private animationService: AnimationService,private translate: TranslateService) {
+    }
+
+  scroll(event: MouseEvent, targetId: string): void {
+    event.preventDefault();
+    this.animationService.scrollToSection(event, targetId);
+    
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);  
+  }
 
 }

27ec559c064668c63110681abe919ddfcacf4180
Denis <denis.d@hotmail.de>
2025-02-02 09:20:02 +0100

    work on policy

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { AnimationService } from '../../services/animation.service';
 
@@ -7,21 +8,31 @@ import { AnimationService } from '../../services/animation.service';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
-    constructor (private animationService: AnimationService,private translate: TranslateService) {
-    }
+  isPrivacyPage: boolean = false; // Flag, um zu prüfen, ob wir auf der Privacy-Route sind
+
+  constructor(private animationService: AnimationService, private translate: TranslateService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Überwache Router-Ereignisse
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Überprüfen, ob die aktuelle Route die Privacy-Route ist
+        this.isPrivacyPage = event.url === '/privacy';
+      }
+    });
+  }
 
   scroll(event: MouseEvent, targetId: string): void {
     event.preventDefault();
     this.animationService.scrollToSection(event, targetId);
-    
   }
 
-  switchLanguage(language: string) {
-    this.translate.use(language);  
+  switchLanguage(language: string): void {
+    this.translate.use(language);
   }
 
 }
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,35 +1,44 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { AnimationService } from '../../services/animation.service';
 import { CommonModule } from '@angular/common';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [TranslateModule, CommonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrls: ['./nav.component.scss']
 })
-export class NavComponent { 
-  constructor (private animationService: AnimationService,private translate: TranslateService) {
+export class NavComponent implements OnInit {
+  burgerMenuOpen = false;
+  isPrivacyPage: boolean = false; // Flag, um zu prüfen, ob wir auf der Privacy-Route sind
+
+  constructor(private animationService: AnimationService, private translate: TranslateService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Überwache Router-Ereignisse
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Überprüfen, ob die aktuelle Route die Privacy-Route ist
+        this.isPrivacyPage = event.url === '/privacy';
+      }
+    });
   }
 
-  burgerMenuOpen = false;
-
   toggleBurgerMenu(): void {
-    this.burgerMenuOpen = !this.burgerMenuOpen;    
+    this.burgerMenuOpen = !this.burgerMenuOpen;
   }
 
   scroll(event: MouseEvent, targetId: string): void {
-    
     event.preventDefault();
-    
     this.animationService.scrollToSection(event, targetId);
     this.burgerMenuOpen = false; // Schließt das Burger-Menü
   }
 
-  switchLanguage(language: string) {
-    this.translate.use(language);  
+  switchLanguage(language: string): void {
+    this.translate.use(language);
   }
-  
 }

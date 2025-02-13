65534d4098c7dfc0fa906c86b0707c689b33a605
Denis-Dudda <denis.d@hotmail.de>
2025-02-13 11:42:11 +0100

    change form at contact

--- src/app/main-page/contact-me/contact-me.component.ts
+++ src/app/main-page/contact-me/contact-me.component.ts
@@ -5,13 +5,15 @@ import { TranslateService } from '@ngx-translate/core';
 import { AnimationService } from '../../services/animation.service';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
   imports: [TranslateModule,
             FormsModule,
-            RouterLink
+            RouterLink,
+            CommonModule
   ],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
